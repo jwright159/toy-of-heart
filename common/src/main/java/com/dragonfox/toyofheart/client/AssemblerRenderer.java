c9e1e076cf1be8946272c2a1a5ca0c40e4bd2b93
Jady Wright <iamjwright159@gmail.com>
2025-06-29 04:26:57 -0400

    Fix assembler part rotation

--- common/src/main/java/com/dragonfox/toyofheart/client/AssemblerRenderer.java
+++ common/src/main/java/com/dragonfox/toyofheart/client/AssemblerRenderer.java
@@ -1,12 +1,14 @@
 package com.dragonfox.toyofheart.client;
 
 import com.dragonfox.toyofheart.Assembler;
+import com.dragonfox.toyofheart.AssemblerBlock;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.block.entity.BlockEntityRenderer;
 import net.minecraft.client.render.block.entity.BlockEntityRendererFactory;
 import net.minecraft.client.render.model.json.ModelTransformationMode;
 import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.util.math.Direction;
 
 public class AssemblerRenderer implements BlockEntityRenderer<Assembler> {
 	public AssemblerRenderer(BlockEntityRendererFactory.Context ctx) {
@@ -17,6 +19,7 @@ public class AssemblerRenderer implements BlockEntityRenderer<Assembler> {
 		matrices.push();
 
 		matrices.translate(0.5, 1.0, 0.5);
+		matrices.multiply(entity.getCachedState().get(AssemblerBlock.FACING).getRotationQuaternion().div(Direction.NORTH.getRotationQuaternion()));
 		MinecraftClient.getInstance().getItemRenderer().renderItem(entity.rootPart, ModelTransformationMode.NONE, light, overlay, matrices, vertexConsumers, entity.getWorld(), 0);
 
 		matrices.pop();
